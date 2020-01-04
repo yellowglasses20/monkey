89e7ea131c02ede51fb5a8b7febfbd384100c915
yellowglasses20 <yano.makoto20@gmail.com>
2020-01-04 19:57:19 +0900

    3.5.2 REPLに整数リテラルの評価追加

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/yellowglasses20/monkey/evaluator"
 	"github.com/yellowglasses20/monkey/lexer"
 	"github.com/yellowglasses20/monkey/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
