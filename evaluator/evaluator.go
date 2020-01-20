d7a329a52cb3b2a87152310b65d5a0b815dcfff7
yellowglasses20 <yano.makoto@gmail.com>
2020-01-20 15:28:19 +0900

    エラー対応

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -194,7 +194,7 @@ func evalProgram(program *ast.Program, env *object.Environment) object.Object {
 	var result object.Object
 
 	for _, statement := range program.Statements {
-		result := Eval(statement, env)
+		result = Eval(statement, env)
 
 		switch result := result.(type) {
 		case *object.ReturnValue:
