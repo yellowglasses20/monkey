f9a1bfe04e602caf4cac3f0799f411fcc4ebebd0
yellowglasses20 <yano.makoto20@gmail.com>
2020-03-16 11:34:18 +0900

    4.2.4 文字列結合追加

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -226,6 +226,10 @@ if (10 > 1) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "Wrold"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -316,6 +320,20 @@ addTwo(2);`
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
