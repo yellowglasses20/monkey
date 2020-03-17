a12daa73141fd4b646369bc2c54dae7599644703
yellowglasses20 <yano.makoto20@gmail.com>
2020-03-17 14:35:54 +0900

    4.6 グランドフィナーレ

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/yellowglasses20/monkey/object"
+import (
+	"fmt"
+
+	"github.com/yellowglasses20/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -90,6 +94,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
